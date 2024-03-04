97cf9242acfaed67526cb2d1a5bf64dde70b04b3
Zorromatamoscas <99506562+Zorromatamoscas@users.noreply.github.com>
2024-03-04 17:30:19 +0100

    Ogre

---
+++ Ogre/src/pugixml-1.13/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/pedro/Documents/GitHub/FORGE_Dependencies/Ogre/src/pugixml-1.13
+# Build directory: C:/Users/pedro/Documents/GitHub/FORGE_Dependencies/Ogre/src/pugixml-1.13
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
