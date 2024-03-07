20edf9edf3ac8e9b014fe7a71f1f8a9480fb8602
Zorromatamoscas <99506562+Zorromatamoscas@users.noreply.github.com>
2024-03-07 18:15:41 +0100

    Update build.bat

--- SDL2/build.bat
+++ SDL2/build.bat
@@ -9,3 +9,6 @@ cmake -S %SDLSRC% -B %BUILDDIR%
 
 msbuild %BUILDDIR%\SDL2.sln /p:configuration=Debug
 msbuild %BUILDDIR%\SDL2.sln /p:configuration=Release
+
+COPY /y %BUILDDIR%\Debug\SDL2d.dll %BINDIRECTION%
+COPY /y %BUILDDIR%\Release\SDL2.dll %BINDIRECTION%
